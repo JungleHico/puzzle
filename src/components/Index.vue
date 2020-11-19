7c952b755978d248959309d7cc18ace22cd84776
WildHico <scnu_zhuxu@163.com>
2020-11-19 20:56:31 +0800

    第一次提交

---
+++ src/components/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <puzzle :imgSrc="require('../assets/puzzle.png')" :difficuty="3"></puzzle>
+    </div>
+</template>
+
+<script>
+import Puzzle from './Puzzle'
+
+export default {
+    components: {
+        Puzzle
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
